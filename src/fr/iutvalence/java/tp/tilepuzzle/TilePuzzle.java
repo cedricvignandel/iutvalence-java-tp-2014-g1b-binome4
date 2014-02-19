e53b149fcac8ce2b08a0b34e368bbe5cbe128e20
bouixl <bouixl@PC-B101-02.iut-valence.fr>
2014-02-19 16:27:11 +0100

    Ajout de la classe réprésentant les parties du jeu

---
+++ src/fr/iutvalence/java/tp/tilepuzzle/TilePuzzle.java
@@ -0,0 +1,25 @@
+package fr.iutvalence.java.tp.tilepuzzle;
+
+/**
+ * Classe principale du jeu
+ */
+public class TilePuzzle
+{
+	/**
+	 * Crée une partie prête à être jouée.
+	 * Cela implique la mise en place d'un plateau et d'un joueur.
+	 */
+	public TilePuzzle()
+	{
+		Plateau plateauDeTilePuzzle = new Plateau();
+		Joueur joueur = new Joueur();
+	}
+
+	/**
+	 * Lance la partie
+	 */
+	public void jouer()
+	{
+		
+	}
+}
