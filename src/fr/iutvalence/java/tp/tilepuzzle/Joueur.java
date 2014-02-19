f4d596f905ba46e3da23c849c49fce52c22bb5f9
bouixl <bouixl@PC-B101-02.iut-valence.fr>
2014-02-19 16:27:48 +0100

    Ajout d'une classe représentant le joueur.

---
+++ src/fr/iutvalence/java/tp/tilepuzzle/Joueur.java
@@ -0,0 +1,15 @@
+package fr.iutvalence.java.tp.tilepuzzle;
+
+/**
+ * Définit un joueur, capable d'interagir avec le plateau.
+ */
+public class Joueur
+{
+	/**
+	 * Crée un nouveau joueur
+	 */
+	public Joueur()
+	{
+		
+	}
+}
