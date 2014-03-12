712b3c22baa8a89c6c617068508008c7e1f3f7ad
bouixl <bouixl@PC-B101-06.iut-valence.fr>
2014-03-12 17:00:43 +0100

    Ajout d'une méthode generant une position sur laquelle jouer (getPosition)

--- src/fr/iutvalence/java/tp/tilepuzzle/Joueur.java
+++ src/fr/iutvalence/java/tp/tilepuzzle/Joueur.java
@@ -1,5 +1,7 @@
 package fr.iutvalence.java.tp.tilepuzzle;
 
+import java.util.Random;
+
 /**
  * Définit un joueur, capable d'interagir avec le plateau.
  */
@@ -11,5 +13,15 @@ public class Joueur
 	public Joueur()
 	{
 		
+	}
+	
+	/**
+	 * Genère une position correspondant à la case à activer
+	 * @return Renvoi la position de la case à activer
+	 */
+	public Position getPosition()
+	{
+		Random machineAlea = new Random();
+		return new Position(machineAlea.nextInt(3), machineAlea.nextInt(3));
 	}
 }
